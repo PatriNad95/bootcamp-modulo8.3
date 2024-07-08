baaa3dca34a181b4fbe17664ec900afa30535971
Patricia Sánchez Nadales <patriciasancheznadales95@gmail.com>
2024-07-08 09:49:56 +0200

    Prueba concepto 2

--- prueba-concepto-2/src/main.ts
+++ prueba-concepto-2/src/main.ts
@@ -1,3 +1,16 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+const carta = document.getElementById("carta");
+if (carta !== null && carta !== undefined && carta instanceof HTMLElement) {
+  carta.addEventListener("click", () => {
+    const img = document.getElementById("imagenCarta");
+    if (img !== null && img !== undefined && img instanceof HTMLImageElement) {
+      img.src =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png";
+    } else {
+      console.error("El elemento con id imagenCarta no es un elemento Imagen");
+    }
+  });
+} else {
+  console.error("El elemento con id carta no es un elemento HTML");
+}
