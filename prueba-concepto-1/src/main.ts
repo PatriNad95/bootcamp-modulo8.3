424b64e038c1e569f45590e1291e56d9dd34d0bb
Patricia Sánchez Nadales <patriciasancheznadales95@gmail.com>
2024-07-08 09:34:15 +0200

    Prueba Concepto 1

--- prueba-concepto-1/src/main.ts
+++ prueba-concepto-1/src/main.ts
@@ -1,3 +1,18 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+export const shuffle = <T>(array: T[]): T[] => {
+  let m = array.length,
+    t,
+    i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+};
+
+// Ejemplo de uso
+const cartas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+console.log(shuffle(cartas));
